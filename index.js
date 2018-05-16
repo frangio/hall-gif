449f8c97f8cd8de784231896a83980b7b2e60db6
Francisco Giordano <frangio.1@gmail.com>
2018-05-16 20:01:48 -0300

    add prettier

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // Load environment variables from `.env` file (optional)
-require('dotenv').config();
+require("dotenv").config();
 
-const slackEventsApi = require('@slack/events-api');
-const SlackClient = require('@slack/client').WebClient;
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const slackEventsApi = require("@slack/events-api");
+const SlackClient = require("@slack/client").WebClient;
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 // *** Initialize event adapter using verification token from environment variables ***
-const slackEvents = slackEventsApi.createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+const slackEvents = slackEventsApi.createSlackEventAdapter(
+  process.env.SLACK_VERIFICATION_TOKEN
+);
 
 const slack = new SlackClient(process.env.SLACK_BOT_TOKEN);
 
-const IMAGE_URL_FILE = '.data/image-url';
+const IMAGE_URL_FILE = ".data/image-url";
 
 function setImage(url) {
   fs.writeFileSync(IMAGE_URL_FILE, url);
@@ -22,38 +24,35 @@ function setImage(url) {
 
 function extractImage(message) {
   console.log(message);
-  if (message.subtype === 'file_share') {
+  if (message.subtype === "file_share") {
     const { file } = message;
-    if (file.mimetype.indexOf('image/') === 0) {
+    if (file.mimetype.indexOf("image/") === 0) {
       return file.url_private;
     } else {
       throw "only images for now plz";
     }
-  } else if (message.subtype === 'message_changed') {
+  } else if (message.subtype === "message_changed") {
     console.log(message.message.attachment);
   } else if (message.attachment) {
     console.log(message.attachment);
-  } 
+  }
 }
-    
-
-
 
 // Initialize an Express application
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const img = fs.readFileSync(IMAGE_URL_FILE);
   res.send(`<img src="${img}">`);
-})
+});
 
 // *** Plug the event adapter into the express app as middleware ***
-app.use('/slack/events', slackEvents.expressMiddleware());
+app.use("/slack/events", slackEvents.expressMiddleware());
 
 // *** Greeting any user that says "hi" ***
-slackEvents.on('message', (message) => {
+slackEvents.on("message", message => {
   try {
     const img = extractImage(message);
     setImage(img);
@@ -63,13 +62,15 @@ slackEvents.on('message', (message) => {
 });
 
 // *** Handle errors ***
-slackEvents.on('error', (error) => {
+slackEvents.on("error", error => {
   if (error.code === slackEventsApi.errorCodes.TOKEN_VERIFICATION_FAILURE) {
     // This error type also has a `body` propery containing the request body which failed verification.
     console.error(`An unverified request was sent to the Slack events Request URL. Request body: \
 ${JSON.stringify(error.body)}`);
   } else {
-    console.error(`An error occurred while handling a Slack event: ${error.message}`);
+    console.error(
+      `An error occurred while handling a Slack event: ${error.message}`
+    );
   }
 });
 
