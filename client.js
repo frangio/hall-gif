b699cbafcf44a2a8dab84c6fed400ad086362a37
Francisco Giordano <frangio.1@gmail.com>
2018-05-19 00:45:34 -0300

    add some level of fault tolerance

--- client.js
+++ client.js
@@ -1,10 +1,25 @@
-async function main() {
+async function refresh() {
   const res = await fetch("/image/poll");
 
   if (res.status === 205) {
     window.location.reload(true);
-  } else {
-    return main();
+  }
+}
+
+function sleep(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function main() {
+  while (true) {
+    try {
+      await refresh();
+    } catch (e) {
+      console.error(e);
+      await sleep(1000);
+    }
   }
 }
 
